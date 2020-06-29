8e5ea3cfd801013c19db7a4b3908d9daac087b5e
Gurudutt <gurudutt.iitr@gmail.com>
2020-06-29 13:43:42 +0530

    Updates

---
+++ precache-manifest.3738f901e9afcdabacb1d24d7252112f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96def4d72455890ecf12926ec961e84",
+    "url": "/react-newsfeed/index.html"
+  },
+  {
+    "revision": "e8fc32da615ce09be22c",
+    "url": "/react-newsfeed/static/css/main.bb2164f2.chunk.css"
+  },
+  {
+    "revision": "5e802780bf0fb4c4d08f",
+    "url": "/react-newsfeed/static/js/2.c1df7a4a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-newsfeed/static/js/2.c1df7a4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8fc32da615ce09be22c",
+    "url": "/react-newsfeed/static/js/main.fd4e9c49.chunk.js"
+  },
+  {
+    "revision": "cdf18514f21204d6cf83",
+    "url": "/react-newsfeed/static/js/runtime-main.9a8bd8ef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-newsfeed/static/media/logo.5d5d9eef.svg"
+  }
+]);
