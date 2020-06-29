3b965876b9fb334448cc7747aaac98c678e8be7f
Gurudutt <gurudutt.iitr@gmail.com>
2020-06-29 23:56:19 +0530

    With google News

--- src/App.js
+++ src/App.js
@@ -10,97 +10,97 @@ function App() {
 
   const [ news, setNews ] = useState();
   // const { loading, setLoading} = useState(true);
-  const everythingUrl = 'http://newsapi.org/v2/everything?sortBy=publishedAt&apiKey=c342e8b7541b42c08fd08283274acb79&pageSize=18';
-  const headlinesUrl = 'https://newsapi.org/v2/top-headlines?sortBy=publishedAt&apiKey=c342e8b7541b42c08fd08283274acb79&pageSize=18&country=in';
-
 
   // News API
-  const [categories, setCategories ] =  useState([
-    {
-      name : 'Headlines',
-      selected  : true
-    },
-    {
-      name : 'Business',
-      selected  : false
-    },
-    {
-      name : 'Entertainment',
-      selected  : false
-    },
-    {
-      name : 'General',
-      selected  : false
-    },
-    {
-      name : 'Health',
-      selected  : false
-    },
-    {
-      name : 'Science',
-      selected  : false
-    },
-    {
-      name : 'Sports',
-      selected  : false
-    },
-    {
-      name : 'Technology',
-      selected  : false
-    }
-]);
+//   const everythingUrl = 'http://newsapi.org/v2/everything?sortBy=publishedAt&apiKey=c342e8b7541b42c08fd08283274acb79&pageSize=18';
+//   const headlinesUrl = 'https://newsapi.org/v2/top-headlines?sortBy=publishedAt&apiKey=c342e8b7541b42c08fd08283274acb79&pageSize=18&country=in';
 
-  function searchByKeyword(query){
-    console.log('\n\n keyword obtained as ', query);
-    // Keywork search, so mark all selcted categories as false
-    let newCategories = categories.map(item => {
-        item.selected = false;
-        return item;
-    })
-    setCategories(newCategories);
+//   const [categories, setCategories ] =  useState([
+//     {
+//       name : 'Headlines',
+//       selected  : true
+//     },
+//     {
+//       name : 'Business',
+//       selected  : false
+//     },
+//     {
+//       name : 'Entertainment',
+//       selected  : false
+//     },
+//     {
+//       name : 'General',
+//       selected  : false
+//     },
+//     {
+//       name : 'Health',
+//       selected  : false
+//     },
+//     {
+//       name : 'Science',
+//       selected  : false
+//     },
+//     {
+//       name : 'Sports',
+//       selected  : false
+//     },
+//     {
+//       name : 'Technology',
+//       selected  : false
+//     }
+// ]);
 
-    Axios.get(`${everythingUrl}&q=${query}`)
-    .then(res =>{
-      console.log('Queried news are ', res.data);
-      setNews(res.data.articles);
-    })
-  }
+//   function searchByKeyword(query){
+//     console.log('\n\n keyword obtained as ', query);
+//     // Keywork search, so mark all selcted categories as false
+//     let newCategories = categories.map(item => {
+//         item.selected = false;
+//         return item;
+//     })
+//     setCategories(newCategories);
 
-  function categorySelected(selectedCategory) {
-    console.log('\n\n Selected Category is ', selectedCategory);
-    let newCategories = categories.map(item => {
-      if(item.name === selectedCategory){
-        item.selected = true;
-      }else{
-        item.selected = false;
-      }
-      return item;
-    })
-    setCategories(newCategories);
-    console.log('\n\n Updated Categories are ', categories);
+//     Axios.get(`${everythingUrl}&q=${query}`)
+//     .then(res =>{
+//       console.log('Queried news are ', res.data);
+//       setNews(res.data.articles);
+//     })
+//   }
 
-    if(selectedCategory === 'Headlines'){
-      Axios.get(`${headlinesUrl}`)
-      .then(res =>{
-        console.log('All Indian new headlines are ', res.data);
-        setNews(res.data.articles);
-      })
-    }else{
-      Axios.get(`${headlinesUrl}&category=${selectedCategory}`)
-      .then(res =>{
-        console.log('Queried news are ', res.data);
-        setNews(res.data.articles);
-      })
-    }
-  }
+//   function categorySelected(selectedCategory) {
+//     console.log('\n\n Selected Category is ', selectedCategory);
+//     let newCategories = categories.map(item => {
+//       if(item.name === selectedCategory){
+//         item.selected = true;
+//       }else{
+//         item.selected = false;
+//       }
+//       return item;
+//     })
+//     setCategories(newCategories);
+//     console.log('\n\n Updated Categories are ', categories);
 
-  useEffect( ()=>{
-    Axios.get(`${headlinesUrl}`)
-      .then(res =>{
-        console.log('All Indian new headlines are ', res.data);
-        setNews(res.data.articles);
-      })
-  },[]);
+//     if(selectedCategory === 'Headlines'){
+//       Axios.get(`${headlinesUrl}`)
+//       .then(res =>{
+//         console.log('All Indian new headlines are ', res.data);
+//         setNews(res.data.articles);
+//       })
+//     }else{
+//       Axios.get(`${headlinesUrl}&category=${selectedCategory}`)
+//       .then(res =>{
+//         console.log('Queried news are ', res.data);
+//         setNews(res.data.articles);
+//       })
+//     }
+//   }
+
+//   useEffect( ()=>{
+//     Axios.get(`${headlinesUrl}`)
+//       .then(res =>{
+//         console.log('All Indian new headlines are ', res.data);
+//         setNews(res.data.articles);
+//       })
+//   },[]);
 
   useEffect( ()=>{
     window.scrollTo(0, 0)
@@ -109,91 +109,91 @@ function App() {
 
 
 // Google News
-// const [categories, setCategories ] =  useState([
-//   {
-//     name : 'Top',
-//     selected  : true
-//   },
-//   {
-//     name : 'World',
-//     selected  : false
-//   },
-//   {
-//     name : 'Nation',
-//     selected  : false
-//   },
-//   {
-//     name : 'Business',
-//     selected  : false
-//   },
-//   {
-//     name : 'Technology',
-//     selected  : false
-//   },
-//   {
-//     name : 'Entertainment',
-//     selected  : false
-//   },
-//   {
-//     name : 'Sports',
-//     selected  : false
-//   },
-//   {
-//     name : 'Science',
-//     selected  : false
-//   },
-//   {
-//     name : 'Health',
-//     selected  : false
-//   }
-// ]);
+const [categories, setCategories ] =  useState([
+  {
+    name : 'Top',
+    selected  : true
+  },
+  {
+    name : 'World',
+    selected  : false
+  },
+  {
+    name : 'Nation',
+    selected  : false
+  },
+  {
+    name : 'Business',
+    selected  : false
+  },
+  {
+    name : 'Technology',
+    selected  : false
+  },
+  {
+    name : 'Entertainment',
+    selected  : false
+  },
+  {
+    name : 'Sports',
+    selected  : false
+  },
+  {
+    name : 'Science',
+    selected  : false
+  },
+  {
+    name : 'Health',
+    selected  : false
+  }
+]);
 
-// function searchByKeyword(query){
-//   console.log('\n\n keyword obtained as ', query);
+function searchByKeyword(query){
+  console.log('\n\n keyword obtained as ', query);
 
-//   Axios.get(`http://gnews.io/api/v3/search?q=${query}&token=9a6674bf15bab6a11b3752f09986d972`)
-//   .then(res =>{
-//     console.log('Queried news are ', res.data);
-//     setNews(res.data.articles);
-//   })
-// }
+  Axios.get(`http://gnews.io/api/v3/search?q=${query}&token=9a6674bf15bab6a11b3752f09986d972`)
+  .then(res =>{
+    console.log('Queried news are ', res.data);
+    setNews(res.data.articles);
+  })
+}
 
-// function categorySelected(category) {
-//   let selectedCategory = category.toLowerCase();
-//   console.log('\n\n Selected Category is ', selectedCategory);
-//   let newCategories = categories.map(item => {
-//     if(item.name === selectedCategory){
-//       item.selected = true;
-//     }else{
-//       item.selected = false;
-//     }
-//     return item;
-//   })
-//   setCategories(newCategories);
-//   console.log('\n\n Updated Categories are ', categories);
+function categorySelected(category) {
+  let selectedCategory = category.toLowerCase();
+  console.log('\n\n Selected Category is ', selectedCategory);
+  let newCategories = categories.map(item => {
+    if(item.name === selectedCategory){
+      item.selected = true;
+    }else{
+      item.selected = false;
+    }
+    return item;
+  })
+  setCategories(newCategories);
+  console.log('\n\n Updated Categories are ', categories);
 
-//   if(selectedCategory === 'Top'){
-//     Axios.get('https://gnews.io/api/v3/top-news?token=9a6674bf15bab6a11b3752f09986d972&country=in')
-//     .then(res =>{
-//       console.log('All Indian new headlines are ', res.data);
-//       setNews(res.data.articles);
-//     })
-//   }else{
-//     Axios.get(`https://gnews.io/api/v3/topics/${selectedCategory}?token=9a6674bf15bab6a11b3752f09986d972`)
-//     .then(res =>{
-//       console.log('Queried news are ', res.data);
-//       setNews(res.data.articles);
-//     })
-//   }
-// }
+  if(selectedCategory === 'Top'){
+    Axios.get('https://gnews.io/api/v3/top-news?token=9a6674bf15bab6a11b3752f09986d972&country=in')
+    .then(res =>{
+      console.log('All Indian new headlines are ', res.data);
+      setNews(res.data.articles);
+    })
+  }else{
+    Axios.get(`https://gnews.io/api/v3/topics/${selectedCategory}?token=9a6674bf15bab6a11b3752f09986d972`)
+    .then(res =>{
+      console.log('Queried news are ', res.data);
+      setNews(res.data.articles);
+    })
+  }
+}
 
-// useEffect( ()=>{
-//   Axios.get('https://gnews.io/api/v3/top-news?token=9a6674bf15bab6a11b3752f09986d972&country=in')
-//     .then(res =>{
-//       console.log('All Indian new headlines are ', res.data);
-//       setNews(res.data.articles);
-//     })
-// },[]);
+useEffect( ()=>{
+  Axios.get('https://gnews.io/api/v3/top-news?token=9a6674bf15bab6a11b3752f09986d972&country=in')
+    .then(res =>{
+      console.log('All Indian new headlines are ', res.data);
+      setNews(res.data.articles);
+    })
+},[]);
 
 
 
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -12,8 +12,8 @@ export default function NewsItem(props) {
 
     return (
              <div className="flex-item card mb-3" onClick={gotoNewsArticle}>
-                        <img className="card-img-top" src={newsItem.urlToImage} alt="News Item" style={{height:'200px'}}/>
-                        {/* <img className="card-img-top" src={newsItem.image} alt="News Item" style={{height:'200px'}}/> */}
+                        {/* <img className="card-img-top" src={newsItem.urlToImage} alt="News Item" style={{height:'200px'}}/> */}
+                        <img className="card-img-top" src={newsItem.image} alt="News Item" style={{height:'200px'}}/>
                         <div className="card-body">
                             <h5 className="card-title">{newsItem.title}</h5>
                             <p className="card-text">{newsItem.description}</p>
