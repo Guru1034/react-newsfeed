64ee7d1513bdd80bc27f2b4ceb87993758b75bf2
Gurudutt <gurudutt.iitr@gmail.com>
2020-06-30 00:23:01 +0530

    Modified footer text

--- src/layouts/Footer.js
+++ src/layouts/Footer.js
@@ -3,7 +3,10 @@ import React from 'react'
 export default function Footer() {
     return (
         <>
-            <p>News feed provided by NewsApi</p>
-        </>
+            <p>Developed By: [Gurudutt] 
+                <a href="mailto:gurudutt.iitr@gmail.com" style={{color:'#60dbfb',padding:'2px 0px 0px 2px',fontSize:'14px'}}>  Email Me
+                </a>
+            </p>
+        </>    
     )
 }
